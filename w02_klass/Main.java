27001487e06c07d3ec4eca38faf2c5bc01b6044d
Liisi Liivik <55079681+lliisi@users.noreply.github.com>
2020-02-18 08:15:22 +0200

    Create Main.java

---
+++ w02_klass/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    
+    public static void main(String[]args){
+        Animal cat=new Animal("Mamo","cat",4,"black");
+        Animal dog=new Animal("Wauwau","dog",2,"white");
+
+        System.out.println(cat);
+    }
+}
